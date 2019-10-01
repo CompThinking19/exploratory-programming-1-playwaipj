0622ac65d8c0037b47dce43d8a51acf5aff5e4da
playwaipj <pjw98@scarletmail.rutgers.edu>
2019-10-01 14:10:18 -0400

    Create exploratory.py

---
+++ exploratory.py
@@ -0,0 +1,13 @@
+#git hub two assignment
+#double double pg 54
+def funcl(x):
+    for num in x:
+        num = 3 * num
+        #this the end of the for loop
+    return x
+print funcl([1,2,3,4])
+f = [5,6,7,8]
+funcl(f)
+print funcl(f)
+G = funcl(f)
+print G
